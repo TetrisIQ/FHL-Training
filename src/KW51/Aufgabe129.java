6c1cc9a0173308e5de6a2376cae7bb2b71ebd24b
Alex <TetrisIQ@web.de>
2016-12-18 13:17:08 +0100

    Fertig
    Change-Id: I5ac02b980525af8272c4c2bcac27088ad8aa4b4a
    Signed-off-by: Alex <TetrisIQ@web.de>
    Rechtschreibfehler sind gewollt und dienen der Belustigung

--- src/KW51/Aufgabe129.java
+++ src/KW51/Aufgabe129.java
@@ -3,57 +3,73 @@ package KW51;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 /**
-	*****alex***** 
-	Autor: Alexander Werner
-	Projekt: KW50
-	Classe: Aufgabe111.java
-	Datum: 12.12.2016
+ * <b>*****TetrisIQ***** </b> <br>
+ * Autor: Alexander Werner <br>
+ * Projekt: FHL-Training <br>
+ * Classe: Aufgabe111.java <br>
+ * Datum: 12.12.2016 <br>
+ * Package: KW51 <br>
  */
 public class Aufgabe129 {
 
-
 	public static void main(String[] args) {
-		 List<String> words = getShortestWords("Dies ist nur ein dummes Beispiel");
-		 System.out.println(words);
-		 // => [ist, nur, ein]
-		 System.out.println(getShortestWords("Dies ist so ein dummes Beispiel"));
-		 // => [so]
-		 System.out.println(getShortestWords(""));
-		 // => []
+		List<String> words = getShortestWords("Dies ist nur ein dummes Beispiel");
+		System.out.println(words);
+		// => [ist, nur, ein]
+		System.out.println(getShortestWords("Dies ist so ein dummes Beispiel"));
+		// => [so]
+		System.out.println(getShortestWords(""));
+		// => []
+		// System.out.println(getShortestWords("aa b cccc d EEE F FFF c c c"));
 
 	}
-	
-	
 
+	/**
+	 * Trennt einen String am Leerzeichen und übergibt ihn an einen Liste
+	 * 
+	 * @param s
+	 *            Ein String mit Leerzeichen getrennt
+	 * @return Den getrennten String als Liste <br>
+	 *         getShortestWords(test, ret.size(), 1, 1);
+	 */
 	public static List<String> getShortestWords(String s) {
-		String[] work = s.split(" ");
+		String[] work = s.split("\\s+"); // \\s+ trennt an mehrzeichen incl.
+											// doppelungen
 		List<String> ret = Arrays.asList(work);
-		List<String> test = new ArrayList<String>(ret);
-		return getShortestWords(test, ret.size(), 1, 1);
+		List<String> words = new ArrayList<String>(ret);
+		return getShortestWords(words, ret.size(), 1, 1);
 	}
-	
 
+	/**
+	 * Vergleicht 2 elemente in einer Liste und Löscht das Längere
+	 * 
+	 * @param ls
+	 *            Eine Liste
+	 * @param origin
+	 *            Die Ursprüngliche Länge der Liste
+	 * @param counter
+	 *            Zählt die Durchgange -> <b> Default: </b> 1
+	 * @param second
+	 *            Zweites Vergleichs Variable -> <b> Default: </b> 1
+	 * @return Die Kurzesten Wörter in einer Liste
+	 */
+	public static List<String> getShortestWords(List<String> ls, int origin, int counter, int second) {
+		List<String> ret = new ArrayList<String>(ls);
+		if (ret.isEmpty() || origin == counter)
+			return ret; // || ret.size() == 1
+		if (ret.get(0).length() > ret.get(second).length())
+			ret.remove(0);
+		else if (ret.get(0).length() < ret.get(second).length())
+			ret.remove(second);
+		else if ((ret.get(0).length() == ret.get(second).length())) {
+			second++;
+		}
+		counter++;
 
-	
-	
-	
-	public static List<String> getShortestWords( List<String> ls,int origin, int counter, int second) {
-			List<String> ret = new ArrayList<String>(ls);
-			if (ret.isEmpty() || origin == counter ) return ret;	//|| ret.size() == 1
-				if (ret.get(0).length() > ret.get(second).length()) ret.remove(0);
-				else if (ret.get(0).length() < ret.get(second).length()) ret.remove(second);
-				else if ((ret.get(0).length() == ret.get(second).length())) {
-					second++;
-				}
-				counter++;
-			
-			return getShortestWords(ret, origin, counter, second);
-		
-	
+		return getShortestWords(ret, origin, counter, second);
+
 	}
 
 }
