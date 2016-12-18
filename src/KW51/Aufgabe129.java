9483a1000534e641b5f3be581067c17e168be44b
Alex <TetrisIQ@web.de>
2016-12-18 12:47:23 +0100

    java doc fehlt noch
    Change-Id: I66cfe92aade237198eec190c9315d354d881967b
    Signed-off-by: Alex <TetrisIQ@web.de>

--- src/KW51/Aufgabe129.java
+++ src/KW51/Aufgabe129.java
@@ -32,60 +32,28 @@ public class Aufgabe129 {
 	public static List<String> getShortestWords(String s) {
 		String[] work = s.split(" ");
 		List<String> ret = Arrays.asList(work);
-//		ret = Arrays.asList(work);
-//		if (s.length() <= 0) return ret;
-		List<String> result = new LinkedList<String>();
-		List<String> test = new LinkedList<String>(ret);
-		return getShortestWords(test, 0, result);
+		List<String> test = new ArrayList<String>(ret);
+		return getShortestWords(test, ret.size(), 1, 1);
 	}
 	
 
-	public static List<String> getShortestWords(List<String> ls, int counter, List<String> result) {
-//		List<String> result = new LinkedList<String>();
-		if (ls.isEmpty() || ls.size() == counter || ls.size() == 1) return result;
-		if (ls.get(0).length() > ls.get(1).length()) {
-			result.add(ls.get(1));
-			ls.remove(0);
-			counter++;
-		}
-		else if (ls.get(0).length() < ls.get(1).length()) {
-			result.add(ls.get(0));
-			ls.remove(1);
-			counter++;
-		}
-		else if (ls.get(0).length() == ls.get(1).length()) {
-			result.add(ls.get(0));
-			result.add(ls.get(1));
-			ls.remove(0);
-			ls.remove(1);
-			counter++;
+
+	
+	
+	
+	public static List<String> getShortestWords( List<String> ls,int origin, int counter, int second) {
+			List<String> ret = new ArrayList<String>(ls);
+			if (ret.isEmpty() || origin == counter ) return ret;	//|| ret.size() == 1
+				if (ret.get(0).length() > ret.get(second).length()) ret.remove(0);
+				else if (ret.get(0).length() < ret.get(second).length()) ret.remove(second);
+				else if ((ret.get(0).length() == ret.get(second).length())) {
+					second++;
+				}
+				counter++;
 			
-		}
+			return getShortestWords(ret, origin, counter, second);
 		
-		return getShortestWords(ls, counter, result);
 	
-
-}
-	
-
-	
-	
-	
-//	public static List<String> getShortestWords( List<String> ls, int counter, int second) {
-//			List<String> ret = new LinkedList<String>(ls);
-//			if (ret.isEmpty() || ret.size() == (counter)  ) return ret;	//|| ret.size() == 1
-//				if (ret.get(0).length() > ret.get(second).length()) ret.remove(0);
-//				else if (ret.get(0).length() < ret.get(second).length()) ret.remove(second);
-//				else if ((ret.get(0).length() == ret.get(second).length())) {
-//					second++;
-//				}
-////				if(!(ret.get(0).length() == ret.get(1).length())) ret.remove(1);
-//
-//				counter++;
-//			
-//			return getShortestWords(ret, counter, second);
-//		
-//	
-//	}
+	}
 
 }
