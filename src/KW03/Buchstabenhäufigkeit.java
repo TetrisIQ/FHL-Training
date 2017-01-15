e9b8830eab9c9628ae2711ac93179afe137ba29c
Alex <TetrisIQ@web.de>
2017-01-15 22:25:06 +0100

    Dies Das Annanas
    Change-Id: Iebc0d51089b575b05783885c3805a40dc8d76c30

--- src/KW03/Buchstabenhäufigkeit.java
+++ src/KW03/Buchstabenhäufigkeit.java
@@ -4,13 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.function.BiPredicate;
 import java.util.function.BinaryOperator;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
@@ -38,20 +35,18 @@ public class Buchstabenhäufigkeit {
     public static String charStat(String string) throws IOException {
 
         String work = getTxt(string);
-        Map<String, List<Double>> dummy = new HashMap<String, List<Double>>();
-        Map<String, List<Double>> map = MapRate(work, dummy, 0, countChar(work));
+        Map<String, Double> dummy = new TreeMap<String, Double>();
+        Map<String, Double> map = MapRate(work, dummy, 0, countChar(work));
         
-
-        return map.toString();
+        return PrettyPrint(map);
     }
-    
-    
-    
+
     public static String PrettyPrint(Map<String, Double> map) {
-        List<Double> keys = new LinkedList<Double>();
-        BiPredicate<Double, Double> test = (a,b) -> a > b ? true : false;
-//        test.test(map.get);
-        return  "";
+        StringBuilder sb = new StringBuilder();
+        map.forEach( (k,v) -> sb.append("- ").append(k).append(" ").append(v).append("\n"));
+        
+        
+        return  sb.toString();
     }
 
     public static double countChar(String string) {
@@ -60,16 +55,15 @@ public class Buchstabenhäufigkeit {
         return dlength;
     }
 
-    public static Map<String, List<Double>> MapRate(String string, Map<String, List<Double>> ret, int i, double max) {
+    public static Map<String, Double> MapRate(String string, Map<String, Double> ret, int i, double max) {
         String[] counts = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
                 "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", ".", ",", "-", ":","ß" };
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", ".", ",", "-", ":", "ß" };
 
         Double j = (double) (string.length() - (string.replaceAll(counts[i], "").length()));
-        BinaryOperator<Double> occur = (a,b) -> (100 /b) * a;
-        List<Double> li = new LinkedList<Double>();
-        li.add(occur.apply(j, max));
-        ret.put(counts[i], li);
+        BinaryOperator<Double> occur = (a, b) -> (100 / b) * a;
+
+        ret.put(counts[i], occur.apply(j, max));
         i++;
 
         return i > counts.length - 1 ? ret : MapRate(string, ret, i, max);
